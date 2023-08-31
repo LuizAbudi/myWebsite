536caeace9e81a09cbc2658414835d29ab44df4a
LuizAbudi <luizabudi@hotmail.com>
2023-08-31 09:27:04 -0300

    editing text

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const typeElements = document.querySelectorAll(".animation h2, .animation p");
 const textToType = [
   "Olá, meu nome é Luiz Gustavo.",
   "Atualmente sou estudante de Ciências da Computação da UTFPR-Campo Mourão. Estudei durante 5 períodos o curso de Engenharia da Computação na UTFPR-Apucarana.",
-  "Atualmente estou estagiando em uma empresa de desenvolvimento de Software, adquirindo conhecimentos de front-end e back-end",
+  "Atualmente estou estagiando em uma empresa de desenvolvimento de Software, adquirindo conhecimentos de front-end e back-end!",
 ];
 let elementIndex = 0;
 let charIndex = 0;
